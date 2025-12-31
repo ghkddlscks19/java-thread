4d53981297cae6f0ac868f3551bb13f61568614e
황인찬 <ghkddlscks19@naver.com>
2025-12-31 21:54:20 +0900

    104. Future6 - 취소

---
+++ src/thread/executor/future/FutureCancelMain.java
@@ -0,0 +1,53 @@
+package thread.executor.future;
+
+import java.util.concurrent.*;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class FutureCancelMain {
+
+//    private static boolean mayInterruptifRunning = true; // 변경
+    private static boolean mayInterruptifRunning = false; // 변경
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        Future<String> future = es.submit(task);
+        log("Future.state: " + future.state());
+
+        // 일정 시간 후 취소 시도
+        sleep(3000);
+
+        // cancel() 호출
+        log("future.cancel(" + mayInterruptifRunning + ") 호출");
+        boolean cancelResult = future.cancel(mayInterruptifRunning);
+        log("cancel(" + mayInterruptifRunning + ") result: " + cancelResult);
+
+        // 결과 확인
+        try {
+            log("Future result: " + future.get());
+        } catch (CancellationException e) {
+            log("Future는 이미 취소되었습니다.");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class MyTask implements Callable<String> {
+        @Override
+        public String call() {
+            try {
+                for(int i = 0; i < 10; i++) {
+                    log("작업 중: " + i);
+                    Thread.sleep(1000); // 1초 동안 sleep
+                }
+            } catch (InterruptedException e) {
+                log("인터럽트 발생");
+                return "Interrupted";
+            }
+
+            return "Completed";
+        }
+    }
+}
